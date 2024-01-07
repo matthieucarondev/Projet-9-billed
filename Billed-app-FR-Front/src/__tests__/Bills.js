26d90123b2f20c2a7dd453921a0d1b8bb48b43b4
Matthieu Caron <104826273+matthieucarondev@users.noreply.github.com>
2024-01-07 20:22:03 +0100

    test modal facture

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -5,6 +5,7 @@ import Bills from "../containers/Bills.js";
 import { bills } from "../fixtures/bills.js";
 import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
+import { sessionStorageMock } from "../__mocks__/sessionStorage.js";
 import mockStore from "../__mocks__/store";
 
 import router from "../app/Router.js";
@@ -14,20 +15,10 @@ describe("Given I am connected as an employee", () => {
   //Quand je suis sur la page Bills(factures)
   describe("When I am on Bills Page", () => {
     //Ensuite, l'icône de la facture dans la disposition verticale doit être mise en surbrillance
-    test("Then bill icon in vertical layout should be highlighted ", async () => {
+    test("Then the invoice icon in the vertical layout should be highlighted and the mail icon is not highlighted ", async () => {
       // Simulation des donnée dans le locale storage
-      Object.defineProperty(window, "localStorage", {
-        value: localStorageMock,
-      });
-      window.localStorage.setItem(
-        "user",
-        JSON.stringify({
-          type: "Employee",
-        })
-      );
-      const root = document.createElement("div");
-      root.setAttribute("id", "root");
-      document.body.append(root);
+      sessionStorageMock("Employee");
+      document.body.innerHTML = '<div id="root"></div>';
       router();
       window.onNavigate(ROUTES_PATH.Bills);
       await waitFor(() => screen.getByTestId("icon-window"));
@@ -51,23 +42,57 @@ describe("Given I am connected as an employee", () => {
     expect(dates).toEqual(datesSorted);
   });
 
- /* action et affichage modale quand clic bouton "nouvelle note de frais" */
- describe("When I click on new bill button ", () => {
-  test("Then a modal should open", () => {
-    sessionStorageMock('Employee')
-    document.body.innerHTML = BillsUI({data: bills,});
-    const onNavigate = (pathname) => {document.body.innerHTML = ROUTES({ pathname, }); };
-    const newBill = new Bills({ document, onNavigate, store: null, bills, localStorage: localStorageMock})          
-    const handleClickNewBill = jest.fn((e) => newBill.handleClickNewBill(e, bills)) 
-    const iconNewBill = screen.getByTestId("btn-new-bill");
-    iconNewBill.addEventListener("click", handleClickNewBill);
-    fireEvent.click(iconNewBill);
-    /* vérification de l'appel de la fonction handleClickNewBill */
-    expect(handleClickNewBill).toHaveBeenCalled();
-     /* vérification de l'affichage de la modale par la présence du noeud DOM
-     id="form-new-bill") */
-    const modale = screen.getAllByTestId("form-new-bill");
-    expect(modale).toBeTruthy();
-  })
-});
-});
+  //action et affichage modale quand clic bouton "nouvelle note de frais"
+  describe("When I click on new bill button ", () => {
+    test("Then a modal should open", () => {
+      sessionStorageMock("Employee");
+      document.body.innerHTML = BillsUI({ data: bills });
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+      const newBill = new Bills({
+        document,
+        onNavigate,
+        store: null,
+        bills,
+        localStorage: localStorageMock,
+      });
+      const handleClickNewBill = jest.fn((e) =>
+        newBill.handleClickNewBill(e, bills)
+      );
+      const iconNewBill = screen.getByTestId("btn-new-bill");
+      iconNewBill.addEventListener("click", handleClickNewBill);
+      fireEvent.click(iconNewBill);
+      //vérification de l'appel de la fonction handleClickNewBill
+      expect(handleClickNewBill).toHaveBeenCalled();
+      //vérification de l'affichage de la modale par la présence du noeud DOM id="form-new-bill")
+
+      const modale = screen.getAllByTestId("form-new-bill");
+      expect(modale).toBeTruthy();
+    });
+  });
+   // test action et affichage modale quand clic icône oeil bleu 
+   describe("When I click on the blue eye icon", () => {
+    test("Then modal should be displayed with its content", async () => {
+      sessionStorageMock("Employee")
+      document.body.innerHTML = '<div id="root"></div>';
+      router()
+      window.onNavigate(ROUTES_PATH.Bills)
+      const onNavigate = (pathname) => { document.body.innerHTML=ROUTES({pathname})}
+    $.fn.modal=jest.fn(function() {this[0].classList.add('show')})
+    const billsList = new Bills({document, onNavigate,store:mockStore,localStorage:null})
+    const bills = await billsList.getBills()
+    document.body.innerHTML=BillsUI({data: bills})
+    const firstEye = screen.getAllByTestId("icon-eye").shift()
+    const handleClickIconEye = jest.fn(()=>billsList.handleClickIconEye(firstEye)) 
+    firstEye.addEventListener("click",handleClickIconEye)
+    userEvent.click(firstEye)
+    // vérification de l'appel de la fonction handleClickIconEye 
+    expect(handleClickIconEye).toBeCalled() 
+     //vérification de l'affichage de la modale par la présence de la classe .show 
+      expect(document.querySelector(".show")).toBeTruthy()
+    });
+
+   });
+  });
+
